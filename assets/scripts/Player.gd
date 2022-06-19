c732b07c6baa2cc978b072f7adf07f73a99d4093
XGit3000 <sebastian.carrier@gmail.com>
2022-06-19 17:07:09 -0400

    done with the basic player movement code
    got jump caching and coyote time to make jumping on time feel more responsive and stuff
    still should clean the code a bit but it's basically done

--- assets/scripts/Player.gd
+++ assets/scripts/Player.gd
@@ -12,8 +12,12 @@ export(float) var jump_cache_time = 50
 export(float) var jump_cached_time = 50
 export(int) var jump_press_time = 0
 export(bool) var jump_cached = false
+export(float) var coyote_time = 100
+export(int) var last_collision_time = 0
 export(NodePath) var jump_timer_node
+export(NodePath) var player_audio_node
 onready var jump_timer : Timer = get_node(jump_timer_node)
+onready var player_audio_player = get_node(player_audio_node)
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
@@ -39,9 +43,9 @@ func _process(delta):
 	var collision = move_and_collide(velocity*delta)
 	if collision:
 		var hit_volume = linear2db(velocity.length()/3000)
+		velocity = move_and_slide(velocity,collision.normal)
 #		velocity = velocity-velocity.project(collision.normal)
-		velocity = velocity.slide(collision.normal)
-		move_and_slide(velocity,collision.normal)
+#		velocity = velocity.slide(collision.normal)
 		if dropping:
 #			print("gonna start playing")
 			var pitch = lerp(0.9,1.1,randf())
@@ -54,7 +58,9 @@ func _process(delta):
 			if collision_time-jump_cached_time <= 1000*jump_cache_time:
 				jump()
 			jump_cached = false
-		
+		last_collision_time = OS.get_ticks_usec()
+	elif not jumping:
+		dropping = true
 #	if collision:
 #		print("COLLLLISIONSNSNSN ", collision)
 #	pass
@@ -65,9 +71,9 @@ func jump():
 
 func play_landing_sound(pitch, volume):
 #	var pitch = lerp(0.9,1.1,randf())
-	$AudioStreamPlayer2D.pitch_scale = pitch
-	$AudioStreamPlayer2D.volume_db = volume
-	$AudioStreamPlayer2D.play()
+	player_audio_player.pitch_scale = pitch
+	player_audio_player.volume_db = volume
+	player_audio_player.play()
 func _input(event):
 #	print("wowoaahaha ", event.as_text())
 #	print(event.)
@@ -81,7 +87,8 @@ func _input(event):
 #		print("\nJUMP")
 #		print(velocity)
 #		velocity.y = -jump_speed
-		if not (jumping or dropping):
+		var coyote_jump = (OS.get_ticks_usec()-last_collision_time<=1000.0*coyote_time)
+		if (not (jumping or dropping)) or (dropping and coyote_jump):
 			jump()
 		elif dropping and !jump_cached:
 			jump_cached = true
