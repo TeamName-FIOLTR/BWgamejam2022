837843eb9905e98b3c62e0d1e98975f8beaad779
Mequam <blue9ja@gmail.com>
2022-06-25 12:15:25 -0400

    udpated the look of level 1 with the background art

---
+++ assets/scripts/appearence/ForGround.gd
@@ -0,0 +1,16 @@
+extends ParallaxLayer
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	add_to_group("Swap Recievers")
+func recieve_swap_color(color)->void:
+	modulate = color
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
